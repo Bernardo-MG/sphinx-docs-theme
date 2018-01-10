36d99211b13bfdb750040513a9bb63fcef761683
Bernardo <programming@wandrell.com>
2018-01-10 20:09:09 +0100

    Updated building configuration

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-import ast
-import re
-import sys
 import io
 from os.path import dirname
 from os.path import join
-from codecs import open
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as test_command
+
+from tox_test_command import ToxTestCommand
+from version_extractor import extract_version_init
+
 
 """
 PyPI configuration module.
@@ -18,8 +17,8 @@ This is prepared for easing the generation of deployment files.
 
 __license__ = 'MIT'
 
-# Regular expression for the version
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+# Source package
+_source_package = 'sphinx_docs_theme/'
 
 # Test requirements
 _tests_require = ['tox']
@@ -32,28 +31,6 @@ def read(*names, **kwargs):
         encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
-# Gets the version for the source folder __init__.py file
-with open('sphinx_docs_theme/__init__.py', 'rb',
-          encoding='utf-8') as f:
-    version_lib = f.read()
-    version_lib = _version_re.search(version_lib).group(1)
-    version_lib = str(ast.literal_eval(version_lib.rstrip()))
-
-
-# For running tox tests
-class _ToxTester(test_command):
-    def finalize_options(self):
-        test_command.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-
-        errcode = tox.cmdline(self.test_args)
-        sys.exit(errcode)
-
 
 setup(
     name='sphinx-docs-theme',
@@ -61,7 +38,7 @@ setup(
     include_package_data=True,
     package_data={
     },
-    version=version_lib,
+    version=extract_version_init(_source_package),
     description='Sphinx Theme for documentation sites.',
     author='Bernardo Martínez Garrido',
     author_email='programming@bernardomg.com',
@@ -95,5 +72,7 @@ setup(
     ],
     tests_require=_tests_require,
     extras_require={'test': _tests_require},
-    cmdclass={'test': _ToxTester},
+    cmdclass={
+        'test': ToxTestCommand
+    },
 )
